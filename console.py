65f2beb2b2d1be8b6936686ee04307253ccc29b5
99omniaashraf <omniaashraf249@gmail.com>
2024-01-12 19:54:29 -0800

    console

--- console.py
+++ console.py
@@ -191,5 +191,5 @@ class HBNBCommand(cmd.cmd):
              setattr(obj, attribute_name, attribute_value)
              obj.save()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
